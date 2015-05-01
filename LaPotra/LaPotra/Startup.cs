d590ca761a98fc99cbadcb86bca5f06766e8fd90
Maynor Calix <calix2007@hotmail.com>
2015-05-01 01:24:35 -0600

    Proyecto inicial.

---
+++ LaPotra/LaPotra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaPotra.Startup))]
+namespace LaPotra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
