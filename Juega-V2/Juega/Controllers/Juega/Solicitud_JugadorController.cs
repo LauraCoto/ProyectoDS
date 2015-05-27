34b67f1dd0d1abef473844c61badf0e84c98c544
Javier Lopez <javierl.1994@gmail.com>
2015-05-27 17:44:44 -0600

    Creando AngularCtrl, Controller MVC, Vista.

---
+++ Juega-V2/Juega/Controllers/Juega/Solicitud_JugadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Juega.Controllers.Juega
+{
+    public class Solicitud_JugadorController : Controller
+    {
+        // GET: Solicitud_Jugador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
