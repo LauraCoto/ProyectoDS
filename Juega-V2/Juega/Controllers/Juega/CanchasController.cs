a669817971d354a46fcb809c38a5c62eab66f36f
David Ramos <davidramos015@gmail.com>
2015-06-03 00:01:41 -0600

    cambiar diseno de canchas, nuevo form. para crearlas

--- Juega-V2/Juega/Controllers/Juega/CanchasController.cs
+++ Juega-V2/Juega/Controllers/Juega/CanchasController.cs
@@ -17,6 +17,10 @@ namespace Juega.Controllers.Juega
             return View();
         }
 
+        public ActionResult Crear()
+        {
+            return View();
+        }
 
         public ActionResult Inicio()
         {
@@ -30,18 +34,18 @@ namespace Juega.Controllers.Juega
                 if (!TieneAcceso())
                     return Resultado_No_Acceso();
 
-               // _db.Configuration.ProxyCreationEnabled = false;
-                var canchas = _db.Cancha.Where(x => x.Activo == true).ToList();
+                // _db.Configuration.ProxyCreationEnabled = false;
+                var canchas = _db.Cancha.Where(x => x.Activo == true).OrderBy(c => c.IdComplejoDeportivo).OrderBy(z => z.FechaCreo).ToList();
 
                 var lista = new List<CanchaModel>();
                 foreach (var item in canchas)
                 {
                     var c = new CanchaModel();
-                    c.Ancho = item.Ancho.ToString();
-                    c.Largo = item.Ancho.ToString();
-                    c.Espectadores = item.NumEspectadores.ToString();
+                    c.Ancho = item.Ancho != null ? Convert.ToInt32(item.Ancho) : 0;
+                    c.Largo = item.Largo != null ? Convert.ToInt32(item.Largo) : 0;
+                    c.Espectadores = item.NumEspectadores != null ? Convert.ToInt32(item.NumEspectadores) : 0;
 
-                    c.IdCancha = item.IdCancha.ToString();
+                    c.IdCancha = item.IdCancha != null ? Convert.ToInt32(item.IdCancha) : 0;
                     c.Nombre = item.Nombre.ToString();
 
                     if (item.ComplejoDeportivo != null)
@@ -67,7 +71,7 @@ namespace Juega.Controllers.Juega
                     return Resultado_No_Acceso();
 
                 _db.Configuration.ProxyCreationEnabled = false;
-                var lista = _db.Cancha.Where(x => x.Activo == true).ToList();
+                var lista = _db.Cancha.Where(x => x.Activo == true).OrderBy(z => z.FechaCreo).ToList();
 
                 return Resultado_Correcto(lista);
             }
@@ -100,6 +104,41 @@ namespace Juega.Controllers.Juega
             catch (Exception e)
             {
                 return Resultado_Exception(e);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Crear(CanchaModel model)
+        {
+
+            try
+            {
+                if (ExisteRegistro(model.Nombre, -1))
+                    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, "Ya existe un complejo con el mismo nombre.");
+
+                var cancha = new Cancha();
+
+                if (model.IdComplejo > 0)
+                    cancha.IdComplejoDeportivo = Convert.ToInt64(model.IdComplejo);
+
+                cancha.Nombre = model.Nombre;
+                cancha.Largo = model.Largo;
+                cancha.Ancho = model.Ancho;
+                cancha.NumEspectadores = model.Espectadores;
+                cancha.Valoracion = 0;
+                cancha.TipoEstado = "1";
+                cancha.Usuario = ObtenerUsuario_Juega();
+                cancha.Activo = true;
+                cancha.FechaCreo = DateTime.Now;
+                _db.Cancha.Add(cancha);
+                _db.SaveChanges();
+
+                return RedirectToAction("Inicio");
+            }
+            catch (Exception e)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, "Ocurrio un error al crear la cancha.");
             }
         }
 
--- Juega-V2/Juega/Models/Juega/CanchaModel.cs
+++ Juega-V2/Juega/Models/Juega/CanchaModel.cs
@@ -8,11 +8,12 @@ namespace Juega.Models.Juega
     public class CanchaModel
     {
 
-        public virtual string IdCancha { get; set; }
+        public virtual int IdCancha { get; set; }
+        public virtual int IdComplejo { get; set; }
         public virtual string Complejo { get; set; }
         public virtual string Nombre { get; set; }
-        public virtual string Largo { get; set; }
-        public virtual string Ancho { get; set; }
-        public virtual string Espectadores { get; set; }
+        public virtual int Largo { get; set; }
+        public virtual int Ancho { get; set; }
+        public virtual int Espectadores { get; set; }
     }
 }
