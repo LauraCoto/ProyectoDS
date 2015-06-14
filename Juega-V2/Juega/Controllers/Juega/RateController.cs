4361b18517635c1b357416706d37de440ed593bc
David Bustillo <hbustillo24@hotmail.com>
2015-06-14 17:02:44 -0600

    Comentarios y Valoraciones - Perfil Canchas

--- Juega-V2/Juega/Controllers/Juega/RateController.cs
+++ Juega-V2/Juega/Controllers/Juega/RateController.cs
@@ -168,5 +168,84 @@ namespace Juega.Controllers.Juega
 
             }
         }
+
+        public JsonResult Canchas (long IdCanchaValorando, int rate, string title, string comment)
+        {
+            try
+            {
+                var cancha = _db.Cancha.FirstOrDefault(x => x.IdCancha == IdCanchaValorando);
+                if (cancha == null)
+                    return Json(new { error = "la cancha que desea evaluar ya no existe.", success = false, id = IdCanchaValorando }, JsonRequestBehavior.AllowGet);
+
+                var usuarioLogueado = ObtenerUsuario_Juega();
+
+                if (usuarioLogueado == null)
+                    return Json(new { error = "Tu sesión ha caducado, vuelve a iniciar sesión.", success = false, id = IdCanchaValorando }, JsonRequestBehavior.AllowGet);
+
+
+                var valoracion = _db.Cancha_Valoracion.FirstOrDefault(x => x.IdUsuarioValoro == usuarioLogueado.IdUsuario &&
+                                                                             x.IdCanchaValorado == IdCanchaValorando);
+
+                if (valoracion == null)
+                {
+                    valoracion = new Cancha_Valoracion();
+                    valoracion.FechaCreo = DateTime.Now;
+                    valoracion.Activo = true;
+                    valoracion.IdUsuarioValoro = usuarioLogueado.IdUsuario;
+                    valoracion.IdCanchaValorado = IdCanchaValorando;
+                    valoracion.Usuario = usuarioLogueado;
+                    valoracion.Valoracion = rate;
+                    valoracion.Comentario = comment;
+                    valoracion.Titulo = title;
+                    _db.Cancha_Valoracion.Add(valoracion);
+
+                    //Actualizar el campo de promedio de la tabla
+                    var queryValoraciones = _db.Cancha_Valoracion.Where(x => x.IdCanchaValorado == IdCanchaValorando && x.Activo == true);
+                    var listaValoraciones = new List<BDD.Cancha_Valoracion>();
+
+                    if (queryValoraciones._IsValid())
+                        listaValoraciones = queryValoraciones.ToList();
+
+                    var totalValoraciones = listaValoraciones.Sum(x => x.Valoracion)._ToDecimal(); //que valor han dado
+                    var div = Convert.ToDecimal((totalValoraciones <= 0 ? 1 : totalValoraciones));
+                    var promedioValoraciones = decimal.Round(totalValoraciones / div, 2, MidpointRounding.AwayFromZero);
+
+                    cancha.Valoracion = promedioValoraciones;
+
+                }
+                else
+                {
+                    //Actualizar el comentario del jugador logueado
+                    valoracion.Comentario = comment;
+                    valoracion.Valoracion = rate;
+
+
+                    //Actualizar el campo de promedio de la tabla
+                    var queryValoraciones = _db.Cancha_Valoracion.Where(x => x.IdCanchaValorado == IdCanchaValorando && x.Activo == true);
+                    var listaValoraciones = new List<BDD.Cancha_Valoracion>();
+
+                    if (queryValoraciones._IsValid())
+                        listaValoraciones = queryValoraciones.ToList();
+
+                    var totalValoraciones = listaValoraciones.Sum(x => x.Valoracion)._ToDecimal(); //que valor han dado
+                    var div = Convert.ToDecimal((totalValoraciones <= 0 ? 1 : totalValoraciones));
+                    var promedioValoraciones = decimal.Round(totalValoraciones / div, 2, MidpointRounding.AwayFromZero);
+
+                    cancha.Valoracion = promedioValoraciones;
+
+
+                    _db.Entry(valoracion).State = System.Data.Entity.EntityState.Modified;
+                }
+
+                _db.SaveChanges();
+                return Json(new { error = "", success = true, id = IdCanchaValorando }, JsonRequestBehavior.AllowGet);
+            }
+
+            catch (System.Exception ex)
+            {
+                return Json(new { error = "Error al guardar valoracion: " + ex.Message, success = false, id = IdCanchaValorando }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
     }
 }
